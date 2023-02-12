ec1764a73ff442bffb9cb6cf6c5fbc05621b23bc
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-02-12 10:08:51 -0800

    WIP

--- scripts/baseline_state.py
+++ scripts/baseline_state.py
@@ -6,10 +6,7 @@ Find districts that minimize population compactness (moment of inertia).
 
 For example:
 
-$ scripts/find_base_districts.py NC congress tract -v -n
-$ scripts/find_base_districts.py NC congress tract -v > results/NC/NC_2020_congress_tract_log.txt
-
-$ scripts/find_base_districts.py NC congress bg -v > results/NC/NC_2020_congress_bg_log.txt
+$ scripts/baseline_state.py NC congress -v > results/NC/NC_2020_congress_log.txt
 
 For documentation, type:
 
@@ -34,11 +31,6 @@ parser.add_argument("state", help="The two-character state code, e.g., NC.", typ
 parser.add_argument(
     "type", help="The type of map: { congress | upper | lower }.", type=str
 )
-parser.add_argument("units", help="The unit of granularity: { tract | bg }.", type=str)
-
-parser.add_argument(
-    "-n", "--noplan", dest="noplan", action="store_true", help="No plan mode"
-)
 parser.add_argument(
     "-v", "--verbose", dest="verbose", action="store_true", help="Verbose mode"
 )
@@ -50,9 +42,7 @@ xx: str = args.state
 fips: str = fips_map[xx]
 plan_type: str = args.type
 n: int = districts_by_state[xx][plan_type]
-units: str = args.units
 
-noplan: bool = args.noplan
 verbose: bool = args.verbose
 
 
@@ -60,32 +50,35 @@ verbose: bool = args.verbose
 
 state_dir: str = xx
 
-features_path: str = path_to_file([data_dir, state_dir]) + file_name(
-    [xx, cycle, units, "data"], "_", "pickle"
+bg_path: str = path_to_file([data_dir, state_dir]) + file_name(
+    [xx, cycle, "bg", "data"], "_", "csv"
 )
-state_path: str = path_to_file([data_dir, state_dir]) + file_name(
-    ["tl", cycle, fips, "state20"], "_"
+block_path: str = path_to_file([data_dir, state_dir]) + file_name(
+    [xx, cycle, "block", "data"], "_", "csv"
 )
 plan_path: str = path_to_file([data_dir, state_dir]) + file_name(
-    [xx, cycle, plan_type, units], "_", "csv"
+    [xx, cycle, plan_type], "_", "csv"
 )
 
+abs_path: str = FileSpec(bg_path).abs_path
+with open(abs_path, "r", encoding="utf-8-sig") as file:
+    print("BG CSV opened")
 
-### FIND DISTRICTS ###
+abs_path: str = FileSpec(block_path).abs_path
+with open(abs_path, "r", encoding="utf-8-sig") as file:
+    print("Block CSV opened")
 
-features: list[Feature] = read_pickle(features_path)
-state_shp: Polygon | MultiPolygon = load_state_shape(state_path, unit_id("state"))
-seeds: list[Coordinate] = PlasticCoordinates(n, state_shp).generate()
+### FIND BASLINE DISTRICTS ###
 
 # TODO -- Integrate Todd's Balzer district solver here.
 # solver: DistrictSolver = DistrictSolver(settings, args.verbose)
 # success: bool = solver.minimize_district_moi()
-plan: list[Assignment] = None
-#
+# plan: list[Assignment] = None
+# #
 
-if plan:
-    print("Success!")
-    if not noplan:
-        write_csv(plan_path, plan)
+# if plan:
+#     print("Success!")
+#     if not noplan:
+#         write_csv(plan_path, plan)
 
 #
