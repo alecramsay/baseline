af3e57eaf3e07d3d2cf4fd94b652709343b5c041
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-02-16 15:25:03 -0800

    Fleshed out nearest neighors

---
+++ scripts/neighbors.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+
+"""
+Find three nearest neighbors for each point
+
+scripts/neighbors.py -s NC
+
+For documentation, type:
+
+scripts/neighbors.py -h
+"""
+
+import argparse
+from argparse import ArgumentParser, Namespace
+import pandas as pd
+import numpy as np
+from sklearn.neighbors import BallTree
+from io import StringIO
+
+from baseline import *
+
+
+### PARSE ARGUMENTS ###
+
+
+def parse_args() -> Namespace:
+    parser: ArgumentParser = argparse.ArgumentParser(
+        description="Find three nearest neighbors for each point"
+    )
+    parser.add_argument(
+        "-s",
+        "--state",
+        default="NC",
+        help="The two-character state code (e.g., NC)",
+        type=str,
+    )
+    parser.add_argument(
+        "-c", "--cycle", default=2020, help="The census cycle (e.g., 2020)", type=int
+    )
+    parser.add_argument(
+        "-u",
+        "--unit",
+        default="block",
+        help="The geographic unit (e.g., block)",
+        type=str,
+    )
+
+    parser.add_argument(
+        "-v", "--verbose", dest="verbose", action="store_true", help="Verbose mode"
+    )
+
+    args: Namespace = parser.parse_args()
+    return args
+
+
+def main() -> None:
+    """Find three nearest neighbors for each point
+
+    Patterned after:
+    https://stackoverflow.com/questions/61952561/how-do-i-find-the-neighbors-of-points-containing-coordinates-in-python
+    """
+    args: Namespace = parse_args()
+
+    xx: str = args.state
+    cycle: int = args.cycle
+    unit: str = args.unit
+
+    verbose: bool = args.verbose
+
+    # Find the nearest neighbors for each point
+
+    # Read the CSV
+    rel_path: str = path_to_file(["data", xx]) + file_name(
+        [xx, str(cycle), unit, "data"], "_", "csv"
+    )
+    df: pd.DataFrame = pd.read_csv(rel_path, sep=",")
+
+    # Find the nearest neighbors for each point
+    tree = BallTree(np.deg2rad(df[["Y", "X"]].values), metric="haversine")
+
+    query_lats: pd.Series = df["Y"]
+    query_lons: pd.Series = df["X"]
+    # NOTE: k=4 because the first match is the point itself
+    distances, indices = tree.query(np.deg2rad(np.c_[query_lats, query_lons]), k=4)
+
+    neighbors: dict[str, list[str]] = {}
+
+    r_km: int = 6371  # multiplier to convert to km (from unit distance)
+    for geoid, d, ind in zip(df["GEOID"], distances, indices):
+        neighbors[geoid] = []
+        for i, index in enumerate(ind):
+            if i == 0:
+                # Skip the first match, which is the point itself
+                continue
+            neighbors[geoid].append(df["GEOID"][index])
+
+    # TODO - Save the neighbors
+    print(f"nodes: {len(neighbors)}")
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+### END ###
