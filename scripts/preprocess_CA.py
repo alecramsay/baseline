eb4e20c2e3f0af63fef1c1cce78b590004a87a5f
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-02-23 10:55:10 -0800

    Preprocessed tract data for CA

---
+++ scripts/preprocess_CA.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+#
+
+"""
+Preprocess data for CA.
+"""
+
+import geopandas
+from geopandas import GeoDataFrame
+
+from baseline import *
+
+
+def main() -> None:
+    """Preprocess the tracts & block groups for CA."""
+
+    commands: list[str] = [
+        "scripts/extract_pop.py -s CA -t -g -i 3 > data/CA/CA_census_log.txt",
+        "scripts/extract_xy.py -s CA -t -g",
+        "scripts/join_feature_data.py -s CA -t -g",
+        "scripts/unpickle_to_csv.py -s CA -u tract",
+        "scripts/unpickle_to_csv.py -s CA -u bg",
+    ]
+    for command in commands:
+        command: str = command.format(xx="CA")
+        os.system(command)
+
+
+if __name__ == "__main__":
+    main()
+
+### END ###
