823c88a6b616cac531b6634a4cc2f4a76867e8e9
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-03-14 07:27:43 -0700

    WIP

--- baseline/baseline.py
+++ baseline/baseline.py
@@ -12,7 +12,48 @@ from .readwrite import *
 
 
 @time_function
-def baseline_state(
+def baseline_state(xx: str, plan_type: str, unit: str, verbose: bool = False) -> None:
+    """Find baseline districts for a state using 'precincts' (VTDs or BGs) & maintaining contiguity"""
+
+    map_label: str = label_map(xx, plan_type)
+    print(f"Generating baseline map for {xx}/{plan_type}:")
+
+    input_csv: str
+    points_csv: str
+    centroids_csv: str
+    initial_csv: str
+    pairs_csv: str
+    balzer_csv: str
+    consolidated_csv: str
+    output_csv: str
+
+    input_csv: str = full_path([data_dir, xx], [xx, cycle, unit, "data"])
+    initial_csv = full_path([data_dir, xx], [xx, cycle, unit, "initial"])
+    pairs_csv = full_path([data_dir, xx], [xx, cycle, unit, "pairs"])
+
+    points_csv = full_path([intermediate_dir, xx], [map_label, unit, "points"])
+    centroids_csv = full_path([intermediate_dir, xx], [map_label, unit, "centroids"])
+    balzer_csv = full_path([intermediate_dir, xx], [map_label, unit, "balzer"])
+    consolidated_csv = full_path(
+        [intermediate_dir, xx], [map_label, unit, "consolidated"]
+    )
+    complete_csv: str = full_path([intermediate_dir, xx], [map_label, unit, "complete"])
+    output_csv: str = full_path([maps_dir], [map_label, unit, "baf"])  # TODO
+
+    make_points(input_csv, points_csv)
+    get_sites(points_csv, initial_csv, centroids_csv)
+    run_balzer_prime(centroids_csv, points_csv, initial_csv, pairs_csv, balzer_csv)
+    consolidate_balzer(balzer_csv, consolidated_csv)
+    # TODO
+    # handle_unassigned(consolidated_csv, points_csv, balzer_csv, complete_csv)
+    # make_assignments(input_csv, complete_csv, output_csv)
+    make_assignments(input_csv, consolidated_csv, output_csv)
+
+    pass
+
+
+@time_function
+def baseline_state_iterative(
     xx: str, plan_type: str, iter_unit: str, finish_unit: str, verbose: bool = False
 ) -> None:
     """Find baseline districts for a state using iter_unit x 100 and finish_unit once."""
@@ -63,7 +104,7 @@ def baseline_state(
 
         make_sites(points_csv, sites_csv, seed, K * N)
         make_initial(sites_csv, points_csv, initial_csv)
-        run_dccvt(sites_csv, points_csv, initial_csv, balzer_csv)
+        run_balzer(sites_csv, points_csv, initial_csv, balzer_csv)
         get_sites(points_csv, balzer_csv, centroids_csv)
 
     # 2 - Find characteristic sites (district centroids) resulting from the random-seed runs.
@@ -92,7 +133,7 @@ def baseline_state(
     combine_centroids(centroids_pattern, points_csv)
     make_sites(points_csv, sites_csv, start + iterations + 1, K * N)
     make_initial(sites_csv, points_csv, initial_csv)
-    run_dccvt(sites_csv, points_csv, initial_csv, balzer_csv)
+    run_balzer(sites_csv, points_csv, initial_csv, balzer_csv)
     get_sites(points_csv, balzer_csv, centroids_csv)
 
     # 3 - Do a final clean up run:
@@ -118,10 +159,10 @@ def baseline_state(
     make_points(input_csv, points_csv)
     # sites_csv = centroids_csv from previous step
     make_initial(centroids_csv, points_csv, initial_csv)
-    run_dccvt(centroids_csv, points_csv, initial_csv, balzer_csv)
+    run_balzer(centroids_csv, points_csv, initial_csv, balzer_csv)
     consolidate_balzer(balzer_csv, consolidated_csv)
     handle_unassigned(consolidated_csv, points_csv, balzer_csv, complete_csv)
-    make_baf(input_csv, complete_csv, output_csv)
+    make_assignments(input_csv, complete_csv, output_csv)
 
     pass
 
@@ -159,7 +200,19 @@ def make_initial(sites_csv: str, points_csv: str, initial_csv: str) -> None:
     os.system(command)
 
 
-def run_dccvt(
+def run_balzer_prime(
+    sites_csv: str, points_csv: str, initial_csv: str, pairs_csv: str, balzer_csv: str
+) -> None:
+    """Run dccvt
+
+    ../../bin/dccvt --sites sites.csv --points points.csv --initial initial.csv --output balzer.csv
+    """
+
+    command: str = f"{dccvt_go}/dccvt --sites {sites_csv} --points {points_csv} --initial {initial_csv} --adjacencies {pairs_csv} --output {balzer_csv}"
+    os.system(command)
+
+
+def run_balzer(
     sites_csv: str, points_csv: str, initial_csv: str, balzer_csv: str
 ) -> None:
     """Run dccvt
@@ -181,8 +234,8 @@ def consolidate_balzer(balzer_csv: str, consolidated_csv: str) -> None:
     os.system(command)
 
 
-def make_baf(input_csv: str, complete_csv: str, baf_csv: str) -> None:
-    """Make a block-assignment file from a file of unique assignments of blocks to districts (sites)
+def make_assignments(input_csv: str, complete_csv: str, baf_csv: str) -> None:
+    """Make an assignment file from a file of unique assignments of VTDs (or blocks) to districts (sites)
 
     python3 redistricting.py postprocess --redistricting_input redistricting.csv --input consolidated.csv --output output.csv
     """
@@ -191,13 +244,13 @@ def make_baf(input_csv: str, complete_csv: str, baf_csv: str) -> None:
     os.system(command)
 
 
-def get_sites(points_csv: str, baf_csv: str, centroids_csv: str) -> None:
-    """Get *ending* sites (district centroids) from assignments
+def get_sites(points_csv: str, assign_csv: str, centroids_csv: str) -> None:
+    """Get sites (district centroids) from assignments
 
     python3 redistricting.py centroids --points point.csv  --assignment balzer.csv --centroids centroids.csv
     """
 
-    command: str = f"python3 {dccvt_py}/redistricting.py centroids --points {points_csv}  --assignment {baf_csv} --centroids {centroids_csv}"
+    command: str = f"python3 {dccvt_py}/redistricting.py centroids --points {points_csv}  --assignment {assign_csv} --centroids {centroids_csv}"
     os.system(command)
 
 
--- scripts/debug.py
+++ scripts/debug.py
@@ -4,23 +4,16 @@
 DEBUG
 """
 
-import argparse
-from argparse import ArgumentParser, Namespace
-from collections import defaultdict
-
 from baseline import *
 
 
 def main() -> None:
-    xx: str = "MI"
+    xx: str = "NC"
+    plan_type: str = "congress"
 
-    rel_path: str = path_to_file([data_dir, xx]) + file_name(
-        [xx, cycle, "water_only"], "_", "csv"
-    )  # GEOID,ALAND,AWATER
-    types: list = [str, int, int]
-    water_precincts: list = [row["GEOID"] for row in read_typed_csv(rel_path, types)]
+    verbose: bool = True
 
-    pass
+    baseline_state(xx, plan_type, "vtd", verbose)
 
 
 if __name__ == "__main__":
