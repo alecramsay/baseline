3ceefefb9444b82782ae0969e7bdf84e79098c41
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-03-16 08:26:53 -0700

    Integrted pyutils

--- baseline/utils.py
+++ baseline/utils.py
@@ -4,50 +4,7 @@
 UTILITIES
 """
 
-import time
-from functools import wraps
-from typing import Any, Callable
-from collections import namedtuple
-
-
-# TIMER DECORATORS
-
-# A decorator to report execution run time for freestanding functions.
-def time_function(func) -> Callable[..., Any]:
-    @wraps(func)
-    def wrapper(*args, **kwargs) -> Any:
-        tic: float = time.perf_counter()
-
-        result: Any = func(*args, **kwargs)
-
-        toc: float = time.perf_counter()
-        print(f"{func.__name__} = {toc - tic: 0.1f} seconds")
-
-        return result
-
-    return wrapper
-
-
-# A decorator to report execution run time for methods w/in classes
-class Timer:
-    def __init__(self) -> None:
-        pass
-
-    @staticmethod
-    def time_method(func) -> Callable[..., Any]:
-        @wraps(func)
-        def wrapper(*args, **kwargs) -> Any:
-            start_counter: float = time.process_time()  # perf_counter?
-
-            result: Any = func(*args, **kwargs)
-
-            end_counter: float = time.process_time()  # perf_counter?
-            counter_time: float = end_counter - start_counter
-            print(func.__name__, "=", "{:.10f}".format(counter_time), "seconds")
-
-            return result
-
-        return wrapper
+from pyutils import Timer, time_function
 
 
 ### END ###
