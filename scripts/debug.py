101690c23c0b772a9112f75c9396513e9dc2aafd
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-04-26 06:09:25 -0700

    WIP

--- scripts/debug.py
+++ scripts/debug.py
@@ -15,12 +15,29 @@ verbose: bool = True
 
 
 def main() -> None:
+    # Unpickle the graph
+
     graph_path: str = path_to_file([temp_dir]) + file_name(
         [xx, cycle, unit, "graph"], "_", "pickle"
     )
     data: dict = read_pickle(graph_path)
 
     g: Graph = Graph(data)
+
+    # Remove water-only precincts
+
+    water_precincts: list = list()
+    if water:
+        rel_path: str = path_to_file([data_dir, xx]) + file_name(
+            [xx, cycle, "water_only"], "_", "csv"
+        )  # GEOID,ALAND,AWATER
+        types: list = [str, int, int]
+        water_precincts = [row["GEOID"] for row in read_csv(rel_path, types)]
+
+        for w in water_precincts:
+            if w in g.nodes():
+                print(f"Removing water-only precinct {w}.")
+                g.remove(w)
 
     print(f"Graph is consistent: {g.is_consistent()}")
     print(f"Graph is connected: {g.is_connected()}")
