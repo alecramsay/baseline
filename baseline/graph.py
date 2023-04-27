291511443b977ab3e0d9abcf69d19e473d3eb183
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-04-27 06:32:56 -0700

    WIP

--- baseline/graph.py
+++ baseline/graph.py
@@ -53,7 +53,8 @@ class Graph:
 
         for node, neighbors in self._data.items():
             for neighbor in neighbors:
-                neighbor_neighbors: list[str | int] = self.neighbors(neighbor)
+                neighbor_neighbors: list[str | int] = self._data[neighbor]
+                # neighbor_neighbors: list[str | int] = self.neighbors(neighbor) DELETE
                 if node in neighbor_neighbors:
                     pass
                 else:
@@ -141,7 +142,8 @@ class Graph:
     def remove(self, node: str | int) -> None:
         """Remove a node from the graph maintaining its connectedness."""
 
-        neighbors: list[str | int] = self.neighbors(node)
+        neighbors: list[str | int] = self._data[node]
+        # neighbors: list[str | int] = self.neighbors(node) DELETE
         for neighbor in neighbors:
             self._data[neighbor].remove(node)
             for new_neighbor in neighbors:
@@ -193,7 +195,8 @@ def border_shapes(
     border: list[str] = list()
 
     for geoid in precincts:
-        for neighbor in precinct_graph.neighbors(geoid):  # neighbor is a GEOID (str)
+        for neighbor in precinct_graph._data[geoid]:
+            # for neighbor in precinct_graph.neighbors(geoid):  # neighbor is a GEOID (str) DELETE
             if neighbor == OUT_OF_STATE:
                 border.append(geoid)
                 break
@@ -218,7 +221,8 @@ def on_border_with(
     candidates: list[str] = list()
 
     for geoid in border:
-        for neighbor in precinct_graph.neighbors(geoid):  # neighbor is a GEOID (str)
+        for neighbor in precinct_graph._data[geoid]:
+            # for neighbor in precinct_graph.neighbors(geoid):  # neighbor is a GEOID (str) DELETE
             if neighbor == OUT_OF_STATE:
                 continue
             if len(districts_by_precinct[str(neighbor)]) > 1:
@@ -239,18 +243,18 @@ def is_connected(geos: list[Any], adjacency: dict[Any, list[Any]]) -> bool:
     visited: set[Any] = set()
 
     all_geos: set[Any] = set(geos)
-    all_geos.discard(OUT_OF_STATE)
+    # all_geos.discard(OUT_OF_STATE)
 
     start: str = next(iter(all_geos))
-    assert start != OUT_OF_STATE
+    # assert start != OUT_OF_STATE
 
     to_process: list[Any] = [start]
     while to_process:
         node: Any = to_process.pop()
         visited.add(node)
         neighbors: list[Any] = adjacency[node]
-        if OUT_OF_STATE in neighbors:
-            neighbors.remove(OUT_OF_STATE)
+        # if OUT_OF_STATE in neighbors:
+        #     neighbors.remove(OUT_OF_STATE)
         neighbors_to_visit: list[Any] = [
             n for n in neighbors if n in all_geos and n not in visited
         ]
