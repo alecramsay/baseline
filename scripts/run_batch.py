391614e7a2ba97d343ab2019db30bdf11474f26c
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-09-02 17:24:20 -0700

    WIP

--- scripts/run_batch.py
+++ scripts/run_batch.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python3
-#
-# SAMPLE CODE
-#
+"""
+Run a batch of commands.
+
+For example:
+
+$ scripts/run_batch.py
+
+"""
 
 import os
 
 from baseline import *
 
-run_states: list[str] = ["NC", "VA", "CO"]
-fail_states: list[str] = ["MD", "AZ", "GA"]
-bg_states: list[str] = ["CA", "OR", "WV"]
-exclude: list[str] = run_states + fail_states + bg_states
-
-states: list[str] = [xx for xx in study_states if xx not in exclude]
-print(states)
-print(len(states))
-
-for xx in states:
-    command: str = f"scripts/extract_blocks_by_vtd.py -s {xx}"
-    # print(command)
-    os.system(command)
 
 pass
